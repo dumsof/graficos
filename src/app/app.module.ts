2b77167785aa4b157f52f639999b9cd81ce0ce6d
Darwin <darwinu34@yahoo.es>
2019-08-28 17:14:43 -0500

    se agrega la libreria de grafico chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+/* libreria de los graficos o chart : url:https://valor-software.com/ng2-charts/  */
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
